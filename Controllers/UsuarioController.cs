4924c3185242ed26b7a5ce439b788b28afd4dbef
Gabriel Pereira Lisboa <gabriel.lisboadv@gmail.com>
2025-09-27 10:08:34 -0300

    adicionando primeiras implementações do projeto

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UsuariosApi.Data.Dtos;
+
+namespace UsuariosApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsuarioController : ControllerBase
+{
+    [HttpGet("cadastrar")]
+    public IActionResult CadastrarUsuario([FromBody] CreateUsuarioDto usuario)
+    {
+
+        return Ok("Usuário cadastrado com sucesso!");
+    }
+}
---
+++ Data/Dtos/CreateUsuarioDto.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UsuariosApi.Data.Dtos;
+
+public class CreateUsuarioDto
+{
+    [Required(ErrorMessage = "O campo Nome é obrigatório")]
+    public string Nome { get; set; }
+
+    [Required(ErrorMessage = "O campo data de nascimento é obrigatório")]
+    public DateTime DataNascimento { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [Required]
+    [Compare("Password")]
+    public string ConfirmPassword { get; set; }
+}
